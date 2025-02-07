0fa22296a2a8a40032a2ca728701a24c2c9a644f
admindisha <dishabhat856@gmail.com>
2025-02-07 19:39:42 +0530

    changes in code and css

--- header-app/src/components/header.jsx
+++ header-app/src/components/header.jsx
@@ -3,6 +3,8 @@ import { IoIosArrowDown } from "react-icons/io";
 import { TfiEmail } from "react-icons/tfi";
 import { GrAnnounce } from "react-icons/gr";
 import { FaChartLine } from "react-icons/fa";
+import { HiOutlineBellAlert } from "react-icons/hi2";
+import { LiaLanguageSolid } from "react-icons/lia";
 
 const Header = () => {
   return (
@@ -10,7 +12,7 @@ const Header = () => {
       <div className="logo">Just<span className="highlight">dial</span></div>
       <nav>
         <ul>
-          <li>EN <IoIosArrowDown /></li>
+          <li> <div style={{color:"blue"}}> <LiaLanguageSolid />EN <IoIosArrowDown /></div></li>
           <li>We are Hiring</li>
           <li>Investor Relations</li>
           <li> <TfiEmail /> Leads</li>
@@ -19,7 +21,7 @@ const Header = () => {
             <span className="business">BUSINESS</span>
             <span className="free-listing"><FaChartLine /> Free Listing</span>
           </li>
-          <li><button className="login">Login / Sign Up</button></li>
+          <li> <HiOutlineBellAlert /> <button className="login">Login / Sign Up</button></li>
         </ul>
       </nav>
     </header>
