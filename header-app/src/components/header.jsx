988296fe0f2774418d822a07c906ee043924ace9
admindisha <dishabhat856@gmail.com>
2025-02-07 10:37:08 +0530

    changes in css

--- header-app/src/components/header.jsx
+++ header-app/src/components/header.jsx
@@ -4,12 +4,8 @@ import './header.css';
 const Header = () => {
   return (
     <div className="header">
-      <div className="logo">Justdial</div>
-      <div className="search-info">Search across '4.7 Crore+' Businesses</div>
-      <div className="location-info">
-        <span className="location">Bettuli, Korwor</span>
-        <span className="search-in">Search in Korwor</span>
-      </div>
+      <div className="logo"><img src="https://akam.cdn.jdmagicbox.com/images/icontent/jd-logo_nw.png" alt="" /></div>
+
     </div>
   );
 };
